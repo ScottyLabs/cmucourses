211ee56ac35ff6cbda49473d760fbe7cd9f743c4
Bradley Teo <bradleyteowj@gmail.com>
2022-04-02 15:07:00 -0400

    Hide unused filters

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -95,29 +95,29 @@ const Filter = () => {
       <div className="pb-3">
         <div className="mb-3 text-lg">Filter by</div>
         <div className="text-sm divide-y">
-          <div className="py-3">
-            <div className="mb-1 font-semibold">Semester</div>
-            <div className="flex flex-col space-y-1 text-sm">
-              <label>
-                <input type="checkbox" className="mr-2" /> Fall 2021
-              </label>
-              <label>
-                <input type="checkbox" className="mr-2" /> Spring 2022
-              </label>
-            </div>
-          </div>
+          {/*<div className="py-3">*/}
+          {/*  <div className="mb-1 font-semibold">Semester</div>*/}
+          {/*  <div className="flex flex-col space-y-1 text-sm">*/}
+          {/*    <label>*/}
+          {/*      <input type="checkbox" className="mr-2" /> Fall 2021*/}
+          {/*    </label>*/}
+          {/*    <label>*/}
+          {/*      <input type="checkbox" className="mr-2" /> Spring 2022*/}
+          {/*    </label>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
-          <div className="py-3">
-            <div className="mb-1 font-semibold">Course Type</div>
-            <div className="flex flex-col space-y-1 text-sm">
-              <label>
-                <input type="checkbox" className="mr-2" /> Mini
-              </label>
-              <label>
-                <input type="checkbox" className="mr-2" /> Non-Mini
-              </label>
-            </div>
-          </div>
+          {/*<div className="py-3">*/}
+          {/*  <div className="mb-1 font-semibold">Course Type</div>*/}
+          {/*  <div className="flex flex-col space-y-1 text-sm">*/}
+          {/*    <label>*/}
+          {/*      <input type="checkbox" className="mr-2" /> Mini*/}
+          {/*    </label>*/}
+          {/*    <label>*/}
+          {/*      <input type="checkbox" className="mr-2" /> Non-Mini*/}
+          {/*    </label>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
 
           <div className="py-3">
             <div className="mb-2 font-semibold">Department</div>
