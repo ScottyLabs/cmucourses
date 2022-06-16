cfb656d660fcb6dd44460524f0f0c404295ecec5
Bradley Teo <bradleyteowj@gmail.com>
2022-06-15 23:13:10 -0400

    Remove unnecessary file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
       },
     },
     colors: {
