6fcdee190e53c1e985f1d3d616f16f13e8bedd0f
Bradley Teo <bradleyteowj@gmail.com>
2022-07-04 18:59:24 -0400

    Fix sidebar button

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = ({ children }) => {
   const dispatch = useAppDispatch();
 
   return open ? (
-    <div className="relative z-20 z-40 w-full md:w-72 lg:w-96">
+    <div className="relative z-20 z-30 w-full md:w-72 lg:w-96">
       <button className="absolute right-3 top-3 z-40 md:top-4 md:right-2">
         <FlushedButton
           onClick={() => dispatch(uiSlice.actions.toggleSidebarOpen())}
@@ -29,13 +29,13 @@ const Sidebar = ({ children }) => {
           </div>
         </FlushedButton>
       </button>
-      <div className="bg-white text-gray-700 inset-0 z-40 space-y-6 p-6 drop-shadow-md md:absolute">
+      <div className="bg-white text-gray-700 inset-0 space-y-6 p-6 drop-shadow-md md:absolute">
         {children}
       </div>
     </div>
   ) : (
     <div className="bg-white relative z-30 h-12 w-full drop-shadow-md md:h-full md:w-16 lg:w-16">
-      <button className="absolute right-3 top-3 z-50">
+      <button className="absolute right-3 top-3 z-40">
         <FlushedButton
           onClick={() => dispatch(uiSlice.actions.toggleSidebarOpen())}
         >
