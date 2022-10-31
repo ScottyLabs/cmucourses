810383e24f03043268c416e0a53592bf1408cab8
Lhwhatever <llinhong.2301+github@gmail.com>
2022-10-30 23:59:55 -0400

    Abstract out logic into a NumericInput component and change strategy
    Instead of storing a potentially unsynced copy of the field value in the
    auxiliary state, we simply just store isBlank. This should resolve any
    sync or input issues on backspace and still prevent the inputting of any
    non-numeric characters.

--- frontend/src/components/Aggregate.tsx
+++ frontend/src/components/Aggregate.tsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SEMESTERS_COUNTED } from "../app/constants";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { userSlice } from "../app/user";
 import { Semester } from "../app/types";
 
 const numericRegex = /\d/;
-const numSemsDispatchableRegex = /\d+/g;
+const numericNonemptyRegex = /\d+/g;
+
+interface NumericInputProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "type" | "value" | "onChange" | "onKeyPress"
+  > {
+  value: number;
+  onChange: (_: number) => void;
+  className: string;
+}
+
+const NumericInput = (props: NumericInputProps) => {
+  const { value, onChange, onBlur, ...other } = props;
+  const [isBlank, setIsBlank] = useState(false);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (numericNonemptyRegex.test(e.target.value)) {
+      setIsBlank(false);
+      onChange(parseInt(e.target.value));
+    } else if (e.target.value === "") {
+      // note: this also happens when there is non-numeric input!
+      // limitation of input[type=number]
+      setIsBlank(true);
+    }
+  };
+
+  const handleBlur: React.FocusEventHandler<HTMLInputElement> = (e) => {
+    if (isBlank) setIsBlank(false);
+    if (onBlur) onBlur(e);
+  };
+
+  const handleKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (!numericRegex.test(e.key)) e.preventDefault();
+  };
+
+  return (
+    <input
+      type="number"
+      value={isBlank ? "" : String(value)}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      onKeyPress={handleKeyPress}
+      {...other}
+    />
+  );
+};
 
 const Aggregate = () => {
   const dispatch = useAppDispatch();
@@ -21,42 +67,6 @@ const Aggregate = () => {
     (state) => state.user.fceAggregation.numSemesters
   );
 
-  // Auxiliary local state required to handle the case when the field is (temporarily) blank
-  const [numSemsField, setNumSemsField] = React.useState<number | undefined>(
-    undefined
-  );
-
-  // To update the global state
-  React.useEffect(() => {
-    setNumSemsField(numSemesters);
-  }, [numSemesters]);
-
-  /* Needed to prevent non-numeric input from being entered by the user. */
-  const handleNumSemsFieldKeyDown: React.KeyboardEventHandler<
-    HTMLInputElement
-  > = (e) => {
-    if (!numeric.test(e.key)) {
-      e.preventDefault();
-    }
-  };
-
-  const handleNumSemsFieldChange: React.ChangeEventHandler<HTMLInputElement> = (
-    e
-  ) => {
-    if (numSemsDispatchableRegex.test(e.target.value)) {
-      setNumSemesters(parseInt(e.target.value));
-    } else if (e.target.value === "") {
-      setNumSemsField(undefined);
-    }
-  };
-
-  // Restore the last valid value if the field is left blank and unfocused
-  const handleNumSemsFieldBlur: React.FocusEventHandler<
-    HTMLInputElement
-  > = () => {
-    if (numSemsField === undefined) setNumSemsField(numSemesters);
-  };
-
   return (
     <div>
       <div className="text-lg">Aggregate FCEs</div>
@@ -65,13 +75,10 @@ const Aggregate = () => {
           <div className="mr-4 whitespace-nowrap text-sm">
             Semesters to Show
           </div>
-          <input
-            type="number"
-            value={numSemsField === undefined ? "" : numSemsField}
+          <NumericInput
+            value={numSemesters}
             className="border-gray-200 min-w-0 flex-auto rounded border px-2 py-1 text-sm bg-transparent"
-            onChange={handleNumSemsFieldChange}
-            onBlur={handleNumSemsFieldBlur}
-            onKeyPress={handleNumSemsFieldKeyDown}
+            onChange={setNumSemesters}
           />
         </div>
         <div className="flex flex-row justify-between text-sm">
