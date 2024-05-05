1ef9dd58b1006f683cbcf00c2e7ba2d34c8a644f
Xavier Lien <xavilien@gmail.com>
2024-05-05 17:10:36 +0000

    Added codespaces support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,7 @@ module.exports = (phase) => {
     env: {
       backendUrl:
         process.env.BACKEND_URL ||
+        process.env.CODESPACES && `https://${process.env.CODESPACE_NAME}-3000.app.github.dev` ||
         (phase === PHASE_DEVELOPMENT_SERVER
           ? "http://localhost:3000"
           : "https://course-tool-backend-2kh6wuzobq-uc.a.run.app"),
