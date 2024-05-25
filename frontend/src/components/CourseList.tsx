4f2435a5881cd93fef92d573bf0269457bfff3bd
Christian Ang <124550545+porkboi@users.noreply.github.com>
2024-05-25 04:43:57 +0000

    Edit: Counter is removed when 0 are found

--- frontend/src/components/CourseList.tsx
+++ frontend/src/components/CourseList.tsx
@@ -27,11 +27,11 @@ const CourseList = ({ courseIDs, children }: Props) => {
 
   return (
     <div className="py-6 px-2 md:px-6">
-    <div className="text-gray-400 mt-3 text-sm pb-2">{courseIDs.length} Saved Courses </div>
       {results.length > 0 ? (
         <div className="space-y-4">
           {/* We found less courses than what we search for, so put a Loading indicator */}
           {courseIDs.length > results.length && <Loading />}
+          <div className="text-gray-400 mt-3 text-sm pb-2">{courseIDs.length} Saved Courses </div>
           {results.map((course) => (
             <CourseCard
               info={course}
