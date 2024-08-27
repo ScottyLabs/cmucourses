d23e09d4a60a14a7521a811ddf341e9133c93c7f
Gabriel Hall <adrastopoulos@gmail.com>
2024-08-26 22:29:55 -0400

    chore: update tailwind config

--- apps/frontend/src/components/Loading.tsx
+++ apps/frontend/src/components/Loading.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import PulseLoader from "react-spinners/PulseLoader";
-
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
 import { useAppSelector } from "~/app/hooks";
-
-/* eslint-disable-next-line */
-const fullConfig: any = resolveConfig(tailwindConfig);
 
 const Loading = () => {
   const darkMode = useAppSelector((state) => state.ui.darkMode);
@@ -15,11 +9,9 @@ const Loading = () => {
     <div className="text-center">
       <PulseLoader
         color={
-          /* eslint-disable */
           darkMode
-            ? fullConfig.theme.colors.zinc[300]
-            : fullConfig.theme.colors.gray[500]
-          /* eslint-enable */
+            ? 'zinc-300'
+            : 'gray-500'
         }
         loading={true}
         size={10}
--- apps/frontend/src/components/StarRating.tsx
+++ apps/frontend/src/components/StarRating.tsx
@@ -1,11 +1,6 @@
 import { useAppSelector } from "~/app/hooks";
 import React from "react";
 import StarRatings from "react-star-ratings";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
-
-/* eslint-disable-next-line */
-const fullConfig: any = resolveConfig(tailwindConfig);
 
 export const StarRating = ({ rating }: { rating: number }) => {
   const darkMode = useAppSelector((state) => state.ui.darkMode);
@@ -18,13 +13,13 @@ export const StarRating = ({ rating }: { rating: number }) => {
       starSpacing="1px"
       starRatedColor={
         darkMode
-          ? fullConfig.theme.colors.zinc[50]
-          : fullConfig.theme.colors.gray[500]
+            ? 'zinc-300'
+            : 'gray-500'
       }
       starEmptyColor={
         darkMode
-          ? fullConfig.theme.colors.zinc[500]
-          : fullConfig.theme.colors.gray[200]
+        ? 'zinc-300'
+        : 'gray-500'
       }
     />
   );
