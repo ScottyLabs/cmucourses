0f6e6734df5733fd9586c19f4b757581c22dbeb1
Xavier Lien <xavilien@gmail.com>
2024-11-09 19:45:41 -0500

    Don't mask searches

--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -12,7 +12,13 @@ export function PHProvider({
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: 'identified_only',
-      capture_pageview: false // Disable automatic pageview capture, as we capture manually
+      capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      session_recording: {
+        maskAllInputs: false,
+        maskInputOptions: {
+          password: true,
+        }
+      }
     })
   }, []);
 
