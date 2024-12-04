77252cd29641d765b6ac4f576db04ae1edace731
Aisha Al Attiyah <aattiyah@andrew.cmu.edu>
2024-12-04 14:34:07 +0300

    create card for pre.post.corequisite tree

---
+++ apps/frontend/src/components/ReqTreeCard.tsx
@@ -0,0 +1,10 @@
+import { Card } from "./Card";
+import React from "react";
+
+export const ReqTreeCard = () => { //add any inputs here
+    return (
+      <Card>
+        <Card.Header>Prerequisite, Corequisite, and Postrequisite Tree</Card.Header>
+      </Card>
+    );
+  };
