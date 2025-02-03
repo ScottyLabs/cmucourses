8ad6c89cae41ff780b09b6c7c859977ed3cccf6d
Anish Pallati <anishp@duck.com>
2025-02-02 21:12:11 -0500

    feat: add clear all button

--- apps/frontend/src/components/Filter.tsx
+++ apps/frontend/src/components/Filter.tsx
@@ -3,8 +3,12 @@ import DepartmentFilter from "./filters/DepartmentFilter";
 import UnitsFilter from "./filters/UnitsFilter";
 import SemestersOfferedFilter from "./filters/SemestersOfferedFilter";
 import LevelFilter from "./filters/LevelFilter";
+import { filtersSlice } from "~/app/filters";
+import { useAppDispatch } from "~/app/hooks";
 
 const Filter = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <div>
       <div className="mb-3 text-lg">Filter by</div>
@@ -13,6 +17,14 @@ const Filter = () => {
         <UnitsFilter />
         <SemestersOfferedFilter />
         <LevelFilter />
+        <div className="flex justify-end">
+          <span
+            className="text-gray-700 underline cursor-pointer"
+            onClick={() => dispatch(filtersSlice.actions.resetFilters())}
+          >
+            clear all
+          </span>
+        </div>
       </div>
     </div>
   );
