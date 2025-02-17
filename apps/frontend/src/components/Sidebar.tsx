fdd5220499f99e09bbae848a5f63c943d908ed94
cirex <cirex314@gmail.com>
2025-02-17 18:18:38 -0500

    Remove thx banner (and a few styling tweaks) (#218)
    * chore: remove thx banner (and a few styling tweaks)
    
    * chore: remove bunlockb

--- apps/frontend/src/components/Sidebar.tsx
+++ apps/frontend/src/components/Sidebar.tsx
@@ -14,8 +14,8 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useAppDispatch();
 
   return open ? (
-    <div className="relative z-30 w-full border-r border-gray-100 md:w-72 lg:w-96 overflow-y-auto bg-white">
-      <button className="absolute right-3 top-3 z-40 md:top-4 md:right-2">
+    <div className="relative border-r border-gray-100 md:w-72 lg:w-96 overflow-y-auto bg-white">
+      <button className="absolute right-3 top-3 md:top-4 md:right-2">
         <FlushedButton
           onClick={() => dispatch(uiSlice.actions.toggleSidebarOpen())}
         >
@@ -29,13 +29,13 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
           </div>
         </FlushedButton>
       </button>
-      <div className="inset-0 space-y-6 p-6 bg-white text-gray-700 md:absolute md:space-y-12">
+      <div className="space-y-6 p-6 bg-white text-gray-700 md:space-y-12">
         {children}
       </div>
     </div>
   ) : (
-    <div className="relative z-30 w-full border-r bg-white border-gray-100 md:h-full md:w-16 lg:w-16">
-      <button className="absolute right-3 top-3 z-40">
+    <div className="relative border-r bg-white border-gray-100 md:w-16 lg:w-16">
+      <button className="absolute right-3 top-3">
         <FlushedButton
           onClick={() => dispatch(uiSlice.actions.toggleSidebarOpen())}
         >
