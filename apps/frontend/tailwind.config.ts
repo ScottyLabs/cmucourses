0240612b142a13cffc60374cd90715b6d0fc5496
Xavier Lien <xavilien@gmail.com>
2025-08-27 00:20:27 -0400

    feat: update dependencies and add corgi banner

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,6 +1,8 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import defaultTheme from "tailwindcss/defaultTheme";
 import nightwind from "nightwind";
+
+const { fontFamily } = defaultTheme;
 
 export default {
   content: [
