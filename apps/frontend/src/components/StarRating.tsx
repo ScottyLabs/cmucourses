edf986be37d578d407c315eec0b1beea642ccb67
Xavier Lien <xavilien@gmail.com>
2025-08-27 19:34:12 -0400

    fix: star ratings

--- apps/frontend/src/components/StarRating.tsx
+++ apps/frontend/src/components/StarRating.tsx
@@ -1,18 +1,29 @@
 import { useAppSelector } from "~/app/hooks";
 import React from "react";
-import StarRatings from "react-star-ratings";
+import { styled } from '@mui/material/styles';
+import { Rating } from '@mui/material';
 import tailwindConfig from "../../tailwind.config.ts";
+
+const StyledRating = styled(Rating)(({ darkMode }: { darkMode: boolean }) => ({
+  '& .MuiRating-iconFilled': {
+    color: darkMode ? tailwindConfig.theme.colors.gray[50] : tailwindConfig.theme.colors.gray[400],
+  },
+  '& .MuiRating-iconHover': {
+    color: darkMode ? tailwindConfig.theme.colors.gray[100] : tailwindConfig.theme.colors.gray[50],
+  },
+}));
 
 export const StarRating = ({ rating }: { rating: number }) => {
   const darkMode = useAppSelector((state) => state.ui.darkMode);
 
   return (
-    <StarRatings
-      rating={rating}
-      starDimension="20px"
-      starSpacing="1px"
-      starRatedColor={darkMode ? tailwindConfig.theme.colors.zinc[50] : tailwindConfig.theme.colors.gray[500]}
-      starEmptyColor={darkMode ? tailwindConfig.theme.colors.zinc[500] : tailwindConfig.theme.colors.gray[50]}
-    />
+    <>
+      <StyledRating
+        value={rating}
+        readOnly
+        precision={0.1}
+        darkMode={darkMode}
+      />
+    </>
   );
 };
