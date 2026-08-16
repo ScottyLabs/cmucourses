749144f99b3a81a0d13ddf17e26ae02a16fcb9a3
Ryan Li <ryanli1366@gmail.com>
2026-08-15 17:54:41 -0700

    feat: base pageview metrics

--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -7,7 +7,7 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only",
+      person_profiles: "always",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
       session_recording: {
         maskAllInputs: false,
